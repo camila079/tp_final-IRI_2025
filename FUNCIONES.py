483ec9c3ebe90b3b17791603e4bca575532cb475
zam <64208228+camila079@users.noreply.github.com>
2025-10-09 10:13:28 -0300

    Update and rename FUNCIONES to FUNCIONES.py

---
+++ FUNCIONES.py
@@ -0,0 +1,22 @@
+def calcular_valores(paciente): #calcula el imc y etc y le das el dic del paciente
+  return paciente_completo #AGREGAs los CALCULOS AL ID PACIENTE
+
+def leer_ecg(): #interrpreta los datos del electrocardiograma
+#valor max y min de la señal
+#promedio de la señal
+
+def triage_pacientes(paciente): #le das el dic del paciente "completo"(con los calculos) y apkica el triage
+  return clasificacion #no sé si prefieren num del 1 al 6 o con colores
+
+def menu()
+  #opciones
+  #no hay return
+
+#-----------------------FUNCIONES QUE GRAFICAN---------------------
+def scatter_imc_pam(paciente): #le pasas la info de un paciente
+  #no hay return solo grafica
+def graficar_ecg(ecg):
+  #no hay return solo grafica
+def grafico_clasificacion(pacientes): #le pasas TODOS los pacientes
+  #no hay return solo grafica
+
