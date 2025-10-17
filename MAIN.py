44660a63b7c0a413dae027bd2fb8f1ffef859bae
zam <64208228+camila079@users.noreply.github.com>
2025-10-17 11:42:01 -0300

    Import numpy and matplotlib, read CSV file

--- MAIN.py
+++ MAIN.py
@@ -1,1 +1,6 @@
-
+import numpy as np
+import matplotlib.pyplot as plt
+with open ("pacientes_final.csv", encoding="utf-8") as archivo:
+  reader=csv.DIctReader(archivo)
+  headers=reader.fieldnames
+  
