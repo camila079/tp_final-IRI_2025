307fc523460801bcffe709ae19bef58818fd3c3d
zam <64208228+camila079@users.noreply.github.com>
2025-10-24 08:46:51 -0300

    Rename functions for clarity in FUNCIONES.py
    Renamed functions for clarity and added comments.

--- FUNCIONES.py
+++ FUNCIONES.py
@@ -13,10 +13,10 @@ def menu()
   #no hay return
 
 #-----------------------FUNCIONES QUE GRAFICAN---------------------
-def scatter_imc_pam(paciente): #le pasas la info de un paciente
+def imc_pam(paciente): #le pasas la info de un paciente SCATTER PLOT
   #no hay return solo grafica
 def graficar_ecg(ecg):
   #no hay return solo grafica
-def grafico_clasificacion(pacientes): #le pasas TODOS los pacientes
+def grafico_clasificacion(pacientes): #le pasas TODOS los pacientes GRAFICO DE BARRAS
   #no hay return solo grafica
 
