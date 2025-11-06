0edc8c1e1f7a95515756097568f6fb58d4e50391
zam <64208228+camila079@users.noreply.github.com>
2025-11-06 15:16:10 -0300

    Update FUNCIONES.py

--- FUNCIONES.py
+++ FUNCIONES.py
@@ -1,14 +1,3 @@
-def calcular_valores(paciente): #calcula el imc y etc y le das el dic del paciente 
-  #separsr en varias funciones para buenas prácticas
-  return paciente_completo #AGREGAs los CALCULOS AL ID PACIENTE
-
-def leer_ecg(): #interrpreta los datos del electrocardiograma
-#valor max y min de la señal
-#promedio de la señal
-#separas en varias funciones para buenas prácticas
-def triage_pacientes(paciente): #le das el dic del paciente "completo"(con los calculos) y apkica el triage
-  return clasificacion #no sé si prefieren num del 1 al 6 o con colores
-
 def menu()
   #opciones
   #no hay return
