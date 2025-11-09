0e2565274e78cb24a16e2563ce6f9c0645de63a0
Anibot09 <anabotello090207@gmail.com>
2025-11-09 00:26:21 -0300

    Update MAIN.py

--- MAIN.py
+++ MAIN.py
@@ -1,4 +1,11 @@
+import MENU as m
 import FUNCIONES as eu
-import GRAFICOS as graf
+entrada_p = "pacientes_final.csv"
+salida_p = "pacientes-2025.cvs"
 
-#llamo a menu
+def main():
+    print("---SISTEMA DE PACIENTES---")
+    eu.sumar_info_pacientes(entrada_p)
+    while True:
+        if m.volver_al_menu():
+                m.menu()
