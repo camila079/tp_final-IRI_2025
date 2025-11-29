cabe525c348fb6cc2c7f274edd3e210b607bdbed
zam <64208228+camila079@users.noreply.github.com>
2025-11-29 12:33:11 -0300

    Update MENU.py

--- MENU.py
+++ MENU.py
@@ -1,117 +1,168 @@
+import matplotlib.pyplot as plt
+import numpy as np
 import FUNCIONES as eu
-import GRAFICOS as gr
+import os
 
-entrada_p = "pacientes_final.csv"
 salida_p = "pacientes_2025.csv"
+entrada_sen = "ecg_manifest.csv"
 
-entrada_s= "ecg_id_pacient.csv"
 
-#--MENU--
+def graficar_lineas_senyales(ruta, entrada_sen):
+    pacientes = eu.archivos_aux(ruta)
+    datos_sen = eu.acceder_ecg(entrada_sen)
+    
+    # Diccionario para guardar IDs por color
+    ids_por_color = {
+        "Azul": None, "Verde": None, "Amarillo": None,
+        "Naranja": None, "Rojo": None, "Negro": None
+    }
 
-def minimenu1():
-    while True:
-        print("\na. Buscar por apellido")
-        print("b. Buscar por ID")
-        sub_op = input("Seleccione una subopción: ").strip().lower()
+    # Asigno el ID de cada color (el primero que aparezca)
+    for paciente in pacientes:
+        color = paciente["clasificacion"]
+        if color in ids_por_color and ids_por_color[color] is None:
+            ids_por_color[color] = paciente["id_paciente"]
 
-        if sub_op == "a":
-            apellido = input("Ingrese el apellido: ")
-            eu.buscar_paciente_por_apellido(apellido)
-            break   
-        elif sub_op == "b":
-            id_p = input("Ingrese el ID del paciente: ")
-            eu.buscar_paciente(id_p)
-            break
-        else: #cada submenú se repite solo cuando opción inválida
-            print("Sub-opción inválida. Elija una de las opciones indicadas.")
+    # Buscar el archivo ECG correspondiente a cada color
+    archivos_ecg = {}
+    for color, id_paciente in ids_por_color.items():
+        if id_paciente:
+            for dato in datos_sen:
+                if id_paciente in dato:
+                    archivos_ecg[color] = dato
+                    break
+    
+    
+    colores = {
+        "Azul": "blue", "Verde": "green", "Amarillo": "yellow",
+        "Naranja": "orange", "Rojo": "red", "Negro": "black"
+    }
 
-def minimenu2(FILE):
+    plt.figure(figsize=(12, 8))
+    i = 1
+    for color, archivo in archivos_ecg.items():
+        if not archivo:
+            print(f"No hay pacientes en estado {color.lower()}")
+            continue
+        #
+        ruta_archivo = os.path.join(
+            r"C:\Users\camila\OneDrive\Documents\proyectos\ecg_signals",
+            archivo
+        )
+        senyales = eu.abrir_archivos(ruta_archivo)
+        y = np.array(senyales)
+
+        plt.subplot(2, 3, i)
+        plt.plot(y, color=colores[color])
+        plt.title(f"ECG - Paciente en estado '{color}'")
+        plt.xlabel("Tiempo (seg.)")
+        plt.ylabel("Amplitud")
+        i += 1
+        
+    plt.tight_layout()
+    plt.show()
+    
+    return
+
+def graficas_barras_estado(ruta):           
+    pacientes = eu.archivos_aux(ruta)
+    cant_azul = 0
+    cant_verde = 0
+    cant_amarillo = 0
+    cant_naranja = 0 
+    cant_rojo = 0
+    cant_negro = 0
+    for paciente in pacientes:
+        if(paciente["clasificacion"] == "Azul"):
+            cant_azul += 1 
+        elif(paciente["clasificacion"] == "Verde"):
+            cant_verde += 1
+        elif(paciente["clasificacion"] == "Amarillo"):
+            cant_amarillo += 1
+        elif(paciente["clasificacion"] == "Naranja"):
+            cant_naranja += 1
+        elif(paciente["clasificacion"] == "Rojo"):
+            cant_rojo += 1
+        elif(paciente["clasificacion"] == "Negro"):
+            cant_negro += 1        
+            
+    fig, ax = plt.subplots()
+    x = np.array(["Estado: Azul", "Estado: Verde", "Estado: Amarillo", "Estado: Naranja", "Estado: Rojo", "Estado: Negro"])
+    y = np.array([cant_azul, cant_verde, cant_amarillo, cant_naranja, cant_rojo, cant_negro])
+    bar_colors = ['tab:blue', 'tab:green', 'yellow', 'tab:orange', 'tab:red', 'black']
+    # Crear barras
+    ax.bar(x, y, color=bar_colors, width=0.6)
+    ax.set_ylabel('Cantidad de pacientes')
+    ax.set_title('Pacientes por estado actual')
+    plt.xticks(fontsize=10, rotation=20) 
+    plt.show()
+    
+    return
+
+def grafico_dispersion(salida_p):
+    try:
+        dict_pacientes = eu.archivos_aux(salida_p)
+        print("Cantidad de pacientes:", len(dict_pacientes))
+        colores = {
+            "Azul": "blue",
+            "Verde": "green",
+            "Amarillo": "yellow",
+            "Naranja": "orange",
+            "Rojo": "red",
+            "Negro": "black"
+        }
+        
+        plt.figure(figsize=(8,6))
+        
+        for color_nombre, color_valor in colores.items():
+            xs = [p["imc"] for p in dict_pacientes if p["clasificacion"] == color_nombre]
+            ys = [p["pam"] for p in dict_pacientes if p["clasificacion"] == color_nombre]
+            if xs and ys:  # si hay pacientes en ese grupo
+                plt.scatter(xs, ys, color=color_valor, label=color_nombre)
+
+        plt.title("Relación IMC vs PAM según clasificación")
+        plt.xlabel("Índice de Masa Corporal (IMC)") #2era pos
+        plt.ylabel("Presión Arterial Media (PAM)") #2 posc
+        plt.grid(True)
+        plt.legend()
+        plt.tight_layout()
+        
+        plt.show()
+    except:
+        print("Error, no se pudo ejecutar")
+        return
+    
+def grafico_dispersion(FILE):
     salida_p = FILE
-    while True:
-        print("\na. Dar de baja por ID ")
-        print("b. Dar de alta")
-        sub_op = input("Seleccione una subopción: ").strip().lower()
+    try:
+        dict_pacientes = eu.archivos_aux(salida_p)
+        print("Cantidad de pacientes:", len(dict_pacientes))
+        colores = {
+            "Azul": "blue",
+            "Verde": "green",
+            "Amarillo": "yellow",
+            "Naranja": "orange",
+            "Rojo": "red",
+            "Negro": "black"
+        }
+        
+        plt.figure(figsize=(8,6))
+        
+        for color_nombre, color_valor in colores.items():
+            xs = [p["imc"] for p in dict_pacientes if p["clasificacion"] == color_nombre]
+            ys = [p["pam"] for p in dict_pacientes if p["clasificacion"] == color_nombre]
+            if xs and ys:  # si hay pacientes en ese grupo
+                plt.scatter(xs, ys, color=color_valor, label=color_nombre)
 
-        if sub_op == "a":
-            id_p = input("Ingrese el ID del paciente a eliminar (solo DÍGITOS): ")
-            eu.eliminar_paciente(id_p,salida_p)
-        elif sub_op == "b":
-            eu.dar_alta_paciente(salida_p)
-        else:
-            print("Sub-opción inválida. Elija una de las opciones indicadas.")
-
-def minimenu3(FILE):
-    salida_p = FILE
-    while True:
-        print("\na. Gráfico clasificación de triage")
-        print("b. Gráfico de ECG")
-        print("c. Gráfico de IMC vs PAM")
-        sub_op = input("Seleccione una subopción: ").strip().lower()
-
-        if sub_op == "a":
-            gr.graficas_barras_estado(salida_p)
-            break
-        elif sub_op == "b":
-            gr.graficar_lineas_senyales(salida_p, entrada_s)
-            break
-        elif sub_op == "c":
-            gr.grafico_dispersion(salida_p)
-            break
-        else:
-            print("Sub-opción inválida. Elija una de las opciones indicadas.")
-
-def menu():
-    salida_p = "pacientes_2025.csv"
-    while True: #siempre vuelve sin reiniciar
-        print("\n--- Menú---")
-        print("1. Buscar paciente.")
-        print("2. Dar de baja/alta.")
-        print("3. Gráficos.")
-        print("4. Pacientes en estado crítico.")
-        print("5. Promedio general de IMC.") 
-        print("6. Promedio de edad de los pacientes.")
-        print("7. Análisis de frecuencia cardíaca.")
-        print("8. Reporte de todos los pacientes.")
-        print("9. Salir.")
-        opcion = input("Seleccione una opción: ").strip()
-        match opcion:
-            case "1":
-                minimenu1()
-            case "2":
-                salida_p = "pacientes_2025.csv"
-                minimenu2(salida_p)
-            case "3":
-                salida_p = "pacientes_2025.csv"
-                minimenu3(salida_p)
-            case "4":
-                salida_p = "pacientes_2025.csv"
-                eu.pacientes_criticos(salida_p)
-            case "5":
-                salida_p = "pacientes_2025.csv"
-                eu.calcular_imc_general(salida_p)
-            case "6":
-                salida_p = "pacientes_2025.csv"
-                eu.promedio_edad(salida_p)
-            case "7":
-                salida_p = "pacientes_2025.csv"
-                eu.extremos_f_card(salida_p)
-            case "8":
-                salida_p = "pacientes_2025.csv"
-                eu.reporte_general(salida_p)
-            case "9":
-                print("Saliendo del programa.")
-            case _:
-                print("Opción inválida. Seleccione una de las opciones del menú.")
-                continue
-        if not volver_al_menu():
-            print("Saliendo del programa.")
-            break
-def volver_al_menu():
-    while True:
-        respuesta = input("¿Ingresar al menú? si/no ").strip().lower()
-        if respuesta in ["si", "sí", "s"]:
-            return True
-        elif respuesta in ["no", "n"]:
-            return False
-        else:
-            print("Respuesta inválida. Responda si/no.")
+        plt.title("Relación IMC vs PAM según clasificación")
+        plt.xlabel("Índice de Masa Corporal (IMC)") #2era pos
+        plt.ylabel("Presión Arterial Media (PAM)") #2 posc
+        plt.grid(True)
+        plt.legend()
+        plt.tight_layout()
+        
+        plt.show()
+    except:
+        print("Error, no se pudo ejecutar")
+    
+    return
